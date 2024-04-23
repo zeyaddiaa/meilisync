e1444185f52712969a04903e0195dbec9c2559df
long2ice <long2ice@gmail.com>
2024-04-23 19:23:41 +0800

    fix: decimal (#97)

--- meilisync/schemas.py
+++ meilisync/schemas.py
@@ -1,4 +1,5 @@
 import datetime
+from decimal import Decimal
 from typing import Optional
 
 from pydantic import BaseModel
@@ -22,6 +23,8 @@ class Event(ProgressEvent):
                 v = int(v.timestamp())
             elif isinstance(v, datetime.date):
                 v = str(v)
+            elif isinstance(v, Decimal):
+                v = float(v)
             if fields_mapping is not None and k in fields_mapping:
                 real_k = fields_mapping[k] or k
                 data[real_k] = v
