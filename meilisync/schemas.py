ef1766bfa4563b5612f3036b554e3497adc0224a
zeyaddiaa <ziad@technologies.info>
2024-08-22 10:25:02 -0700

    year 0 fix v2

--- meilisync/schemas.py
+++ meilisync/schemas.py
@@ -1,15 +1,11 @@
 import datetime
 from decimal import Decimal
 from typing import Optional
-
 from pydantic import BaseModel
-
 from meilisync.enums import EventType
-
 
 class ProgressEvent(BaseModel):
     progress: dict | None = None
-
 
 class Event(ProgressEvent):
     type: EventType
@@ -19,15 +15,23 @@ class Event(ProgressEvent):
     def mapping_data(self, fields_mapping: Optional[dict] = None):
         data = {}
         for k, v in self.data.items():
-            if isinstance(v, datetime.datetime):
-                if v.year > 0:
-                    v = int(v.timestamp())
-                else:
-                    v = None
-            elif isinstance(v, datetime.date):
-                v = str(v)
-            elif isinstance(v, Decimal):
-                v = float(v)
+            try:
+                if isinstance(v, datetime.datetime):
+                    if v.year > 0:
+                        v = int(v.timestamp())
+                    else:
+                        raise ValueError(f"Invalid year {v.year} in datetime: {v}")
+                elif isinstance(v, datetime.date):
+                    if v.year > 0:
+                        v = str(v)
+                    else:
+                        raise ValueError(f"Invalid year {v.year} in date: {v}")
+                elif isinstance(v, Decimal):
+                    v = float(v)
+            except Exception as e:
+                print(f"Error processing field '{k}': {e}")
+                v = 0
+
             if fields_mapping is not None and k in fields_mapping:
                 real_k = fields_mapping[k] or k
                 data[real_k] = v
