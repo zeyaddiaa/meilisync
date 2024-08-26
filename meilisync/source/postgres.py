c19163604be3aac1530e6b6cb3c93599704d83cd
zeyaddiaa <ziad@technologies.info>
2024-08-26 15:36:03 +0300

    Add get_count

--- meilisync/source/postgres.py
+++ meilisync/source/postgres.py
@@ -143,6 +143,12 @@ class Postgres(Source):
         
         asyncio.create_task(self.queue.put(event))
 
+    async def get_count(self, sync: Sync):
+        with self.conn_dict.cursor() as cur:
+            cur.execute(f"SELECT COUNT(*) FROM \"{sync.table}\"")
+            ret = cur.fetchone()
+            return ret[0]
+
     async def __aiter__(self):
         self.queue = Queue()
         
